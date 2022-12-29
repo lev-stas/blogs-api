20e2756bc041fe1ec692b29fa69e63b7ee4faabb
Stanislav Levchenko <staslevchenko@protonmail.com>
2022-12-28 23:52:34 -0300

    enabled all cors via *

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,9 @@ const port = process.env.PORT;
 const welcomeMessage:string = 'It Would Be Great to Get a Welcome Drink Here!';
 
 
-app.use(cors())
+app.use(cors({
+    origin:"*"
+}))
 app.use(parser)
 app.use(cookiesParser)
 
